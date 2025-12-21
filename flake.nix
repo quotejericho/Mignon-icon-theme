55eee0d0593b7fe2808ed9b3639453363c3191e9
Quote <123989069+Silk-OT@users.noreply.github.com>
2025-12-21 01:35:37 -0500

    Refactor outputs in flake.nix
    Refactor outputs structure for better readability.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "Flat, Pastel, Cute Icons for Linux";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    
+
     icon_src = {
       flake = false;
       #url = "github:IgorFerreiraMoraes/Mignon-icon-theme";
@@ -11,17 +11,23 @@
     };
   };
 
-  outputs = {self, nixpkgs, ...}@inputs: let
-    forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
-  in {
-    packages = forAllSys (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mignon_pastel = pkgs.callPackage ./. {
-        inherit (inputs) icon_src;
-      };
-      in {
-        default = mignon_pastel;
-      }
-    );
-  };
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+    in
+    {
+      packages = forAllSys (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          mignon_pastel = pkgs.callPackage ./. {
+            inherit (inputs) icon_src;
+          };
+        in
+        {
+          default = mignon_pastel;
+        }
+      );
+    };
 }
